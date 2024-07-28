5cc68e7347c9bd306396c0f9b3ab78b031099d8f
yar4ess <yarik.esev.93@inbox.ru>
2024-07-28 08:12:57 +0300

    added account field

--- .apex/src/lwc/orderManagement/orderManagement.js
+++ .apex/src/lwc/orderManagement/orderManagement.js
@@ -1,6 +1,5 @@
 import { LightningElement, track, wire } from 'lwc';
-//import getAccountDetails from '@salesforce/apex/OrderManagementController.getAccountDetails';
-//import getProductList from '@salesforce/apex/OrderManagementController.getProductList';
+import getAccountDetails from '@salesforce/apex/AccountController.getAccountDetails';
 
 export default class OrderManagement extends LightningElement {
     @track accountName;
@@ -8,41 +7,14 @@ export default class OrderManagement extends LightningElement {
     @track typeOptions = [];
     @track familyOptions = [];
     @track filteredProducts = [];
-//
-//    @wire(getAccountDetails) wiredAccount({ error, data }) {
-//        if (data) {
-//            this.accountName = data.Name;
-//            this.accountNumber = data.AccountNumber;
-//        } else if (error) {
-//            // Обработка ошибок
-//        }
-//    }
-//
-//    @wire(getProductList) wiredProducts({ error, data }) {
-//        if (data) {
-//            this.filteredProducts = data;
-//        } else if (error) {
-//            // Обработка ошибок
-//        }
-//    }
-
-    handleTypeChange(event) {
-        // Логика фильтрации по Type
-    }
-
-    handleFamilyChange(event) {
-        // Логика фильтрации по Family
-    }
-
-    handleSearch(event) {
-        // Логика поиска
-    }
-
-    handleCreateProduct() {
-        // Логика для создания продукта
-    }
-
-    handleOpenCart() {
-        // Логика для открытия корзины
+    // Получение данных об аккаунте
+    @wire(getAccountDetails, { accountId: '$recordId' })
+    wiredAccount({ error, data }) {
+        if (data) {
+            this.accountName = data.Name;
+            this.accountNumber = data.AccountNumber;
+        } else if (error) {
+            console.error(error);
+        }
     }
 }
--- src/lwc/orderManagement/orderManagement.js
+++ src/lwc/orderManagement/orderManagement.js
@@ -1,8 +1,20 @@
 import { LightningElement, track, wire } from 'lwc';
+import getAccountDetails from '@salesforce/apex/AccountController.getAccountDetails';
+
 export default class OrderManagement extends LightningElement {
     @track accountName;
     @track accountNumber;
     @track typeOptions = [];
     @track familyOptions = [];
     @track filteredProducts = [];
+    // Получение данных об аккаунте
+    @wire(getAccountDetails, { accountId: '$recordId' })
+    wiredAccount({ error, data }) {
+        if (data) {
+            this.accountName = data.Name;
+            this.accountNumber = data.AccountNumber;
+        } else if (error) {
+            console.error(error);
+        }
+    }
 }
