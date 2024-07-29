2e89bba3a72c8d6d247b303c5bf2ebbf08081969
yar4ess <yarik.esev.93@inbox.ru>
2024-07-29 14:09:09 +0300

    added product controller

--- src/lwc/orderManagement/orderManagement.js
+++ src/lwc/orderManagement/orderManagement.js
@@ -1,12 +1,24 @@
 import { LightningElement, track, wire } from 'lwc';
 import getAccountDetails from '@salesforce/apex/AccountController.getAccountDetails';
+import getFilteredProducts from '@salesforce/apex/ProductController.getFilteredProducts';
 
 export default class OrderManagement extends LightningElement {
     @track accountName;
     @track accountNumber;
-    @track typeOptions = [];
-    @track familyOptions = [];
+    @track typeOptions = [
+        { label: 'All Types', value: '' },
+        { label: 'Type1', value: 'Type1' },
+        { label: 'Type2', value: 'Type2' }
+    ];
+
+    @track familyOptions = [
+        { label: 'All Families', value: '' },
+        { label: 'Family1', value: 'Family1' },
+        { label: 'Family2', value: 'Family2' },
+    ];
     @track filteredProducts = [];
+    selectedFamily = '';
+    selectedType = '';
     // Получение данных об аккаунте
     @wire(getAccountDetails, { accountId: '$recordId' })
     wiredAccount({ error, data }) {
@@ -16,5 +28,36 @@ export default class OrderManagement extends LightningElement {
         } else if (error) {
             console.error(error);
         }
+    }
+    handleCreateProduct() {
+        console.log('Create Product button clicked');
+    }
+
+    handleOpenCart() {
+        console.log('Cart button clicked');
+    }
+
+    connectedCallback() {
+        this.fetchProducts();
+    }
+
+    handleTypeChange(event) {
+        this.selectedType = event.detail.value;
+        this.fetchProducts();
+    }
+
+    handleFamilyChange(event) {
+        this.selectedFamily = event.detail.value;
+        this.fetchProducts();
+    }
+
+    fetchProducts() {
+        getFilteredProducts({ family: this.selectedFamily, type: this.selectedType })
+            .then(result => {
+                this.filteredProducts = result;
+            })
+            .catch(error => {
+                console.error('Error fetching products:', error);
+            });
     }
 }
